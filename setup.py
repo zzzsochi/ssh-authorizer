3a08cb6592b31c20ce811f40a873c09f2f2e7b6c
Alexander Zelenyak <zzz.sochi@gmail.com>
2012-12-19 00:21:51 +0400

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
 
-setup(name='ssh_authorizer',
-      description='Manager for remote ~/.ssh/authorized_keys.',
-      author='Alexander Zelenyak aka ZZZ',
-      author_email='zzz.sochi@gmail.com',
-      url='http://github.com/zzzsochi/ssh_authorized/',
-      version='1.0',
-      packages=['ssh_authorizer'],
-      scripts=['scripts/ssh-authorizer'],
-      install_requires=['sh'],
+setup(
+        name='ssh_authorizer',
+        description='Manager for remote ~/.ssh/authorized_keys.',
+        author='Alexander Zelenyak aka ZZZ',
+        author_email='zzz.sochi@gmail.com',
+        url='http://github.com/zzzsochi/ssh_authorized/',
+        version='1.0',
+        packages=['ssh_authorizer'],
+        scripts=['scripts/ssh-authorizer'],
+        install_requires=['sh'],
+        classifiers=[
+                'Operating System :: Unix',
+                'Programming Language :: Python :: 3',
+                'Programming Language :: Python :: 3.3',
+              ],
 )
