c688a0e392c49bb9ea142142c6334e2df92416e3
Alexander Zelenyak <zzz.sochi@gmail.com>
2012-12-19 00:24:15 +0400

    Fix url in setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
         name='ssh_authorizer',
+        version='1.0',
         description='Manager for remote ~/.ssh/authorized_keys.',
         author='Alexander Zelenyak aka ZZZ',
         author_email='zzz.sochi@gmail.com',
-        url='http://github.com/zzzsochi/ssh_authorized/',
-        version='1.0',
+        url='https://github.com/zzzsochi/ssh-authorizer/',
         packages=['ssh_authorizer'],
         scripts=['scripts/ssh-authorizer'],
         install_requires=['sh'],
@@ -14,5 +14,5 @@ setup(
                 'Operating System :: Unix',
                 'Programming Language :: Python :: 3',
                 'Programming Language :: Python :: 3.3',
-              ],
+        ],
 )
